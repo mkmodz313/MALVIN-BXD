6b2f1cd0d96b24168b0f93d0d7b22c396b7e8db5
mkmodz313 <hangustudio93@gmail.com>
2025-09-16 10:02:41 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "malvin~k6lSFIga#UBeEpxdVw_XHPy_Nq93pM621JP8u1zjA4Z0pdTQCtVU",  // add your Session Id here
   
-  OWNER_NUMBER: "9203327011312", // put your phone number here
+  OWNER_NUMBER: "9203307499677", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
